4adbf1b015148d20a142bb5af00e686cbc99606b
gilad kahala <gilad@askpillar.com>
2021-08-29 09:52:56 -0400

    first

---
+++ app.py
@@ -0,0 +1,70 @@
+from typing import Type, Optional
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_marshmallow import Marshmallow
+from sqlalchemy import MetaData
+from flask_login import LoginManager
+import logging
+from config import Config, LocalConfig, TestingConfig, StagingConfig, ProductionConfig
+
+
+DEVELOPMENT = "development"
+TESTING = "testing"
+STAGING = "staging"
+PRODUCTION = "production"
+
+
+app = Flask(
+    __name__, static_folder="./client/dist", template_folder="./client/build"
+)  # type: Flask
+
+config_map = {
+    DEVELOPMENT: LocalConfig,
+    TESTING: TestingConfig,
+    STAGING: StagingConfig,
+    PRODUCTION: ProductionConfig,
+}
+
+config = config_map[app.env]  # type: ignore
+app.config.from_object(config)
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
+def _get_logger(log_level: str) -> logging.Logger:
+    logging.basicConfig(
+        level=log_level, format="%(asctime)s - %(levelname)s - %(message)s"
+    )
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+def get_env() -> Optional[str]:
+    return app.env
+
+
+metadata = MetaData(naming_convention=convention)  # # type: MetaData
+db = SQLAlchemy(app, metadata=metadata)  # type: SQLAlchemy
+login_manager = LoginManager()  # type: LoginManager
+login_manager.init_app(app)
+migrate = Migrate(app, db)  # type: Migrate
+ma = Marshmallow(app)  # type: Marshmallow
+logger = _get_logger(config.LOG_LEVEL)  # type: logging.Logger
+
+
+# Models
+from models.user import User
+from models.plaid_item import PlaidItem
+from models.plaid_account import PlaidAccount
+
+# Routes
+import controller.user_routes
+import controller.react_routes
+import controller.plaid_routes
---
+++ config.py
@@ -0,0 +1,38 @@
+import os
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    DEBUG = False
+    TESTING = False
+    LOG_LEVEL = "INFO"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "thisisasecretkey")
+
+    PLAID_CLIENT_ID = os.getenv("PLAID_CLIENT_ID", "5ae9039e900e950013499ad4")
+    PLAID_SECRET = os.getenv("PLAID_SECRET", "b9b6191d2b3e6b4d958e70f7c80a94")
+    PLAID_PUBLIC_KEY = os.getenv("PLAID_PUBLIC_KEY", "a004a070f0629da694fbae916414f3")
+    PLAID_ENV = os.getenv("PLAID_ENV", "sandbox")
+
+
+class LocalConfig(Config):
+    DEBUG = True
+    LOG_LEVEL = "DEBUG"
+    SQLALCHEMY_DATABASE_URI = "postgres://localhost/village"
+
+
+class TestingConfig(Config):
+    ENV = "testing"
+    TESTING = True
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL", "postgresql://localhost/village_test"
+    )
+
+
+class StagingConfig(Config):
+    LOG_LEVEL = "INFO"
+
+
+class ProductionConfig(Config):
+    LOG_LEVEL = "INFO"
