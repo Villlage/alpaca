1ce691d41b1d8ccdd45eb95ff62a7a402e5c8dcb
gilad kahala <gilad@askpillar.com>
2021-08-29 10:57:55 -0400

    [psql fix]

--- config.py
+++ config.py
@@ -3,6 +3,9 @@ import os
 
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+
     DEBUG = False
     TESTING = False
     LOG_LEVEL = "INFO"
