d8f7acc896bb1f20a835840416c6675217fb9038
gilad kahala <gilad@askpillar.com>
2021-09-19 07:33:12 -0400

    [buy/sell functionality]

--- controller/tests/test_trading_routes.py
+++ controller/tests/test_trading_routes.py
@@ -28,4 +28,58 @@ class TestTradeRoutes:
             resp = client.get("/status")
 
             assert resp.status_code == 200
-            assert resp.json['message'] == 'hello'
+            assert resp.json['message'] == 'Operational I am'
+
+
+class TestBuy:
+    @pytest.fixture()
+    def mock_buy_stock(self, mocker) -> None:
+        return mocker.patch(
+            "controller.trading_routes.buy_stock",
+            return_value="",
+        )
+
+    def test_buy_stock(self, mock_buy_stock):
+        with app.test_client() as client:
+            resp = client.post(
+                "/buy",
+                data=json.dumps(dict(ticker="GOOG", close=123)),
+                content_type="application/json",
+            )
+
+            assert resp.status_code == 200
+
+
+class TestSell:
+    @pytest.fixture()
+    def mock_sell_stock(self, mocker) -> None:
+        return mocker.patch(
+            "controller.trading_routes.sell_stock",
+            return_value="",
+        )
+
+    def test_sell_stock(self, mock_sell_stock):
+        with app.test_client() as client:
+            resp = client.post(
+                "/sell",
+                data=json.dumps(dict(ticker="GOOG", close=123)),
+                content_type="application/json",
+            )
+
+            assert resp.status_code == 200
+
+class TestPortfolio:
+    @pytest.fixture()
+    def mock_portfolio(self, mocker) -> None:
+        return mocker.patch(
+            "controller.trading_routes.get_postitions",
+            return_value=[{'AAPL':123}, {'GOOG':321}],
+        )
+
+    def test_portfolio(self, mock_portfolio):
+        with app.test_client() as client:
+            resp = client.get("/portfolio")
+
+            assert resp.status_code == 200
+            assert resp.json == [{'AAPL':123}, {'GOOG':321}]
+
